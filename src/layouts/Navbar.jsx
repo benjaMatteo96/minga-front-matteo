891d1bebded43a065799445ef5f647346306f323
benjaMatteo96 <127891765+benjaMatteo96@users.noreply.github.com>
2023-08-28 12:39:47 -0400

    add useState

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import Display from '../components/Display'
 
 function Navbar() {
   const [display, setDisplay] = useState(false)
@@ -10,13 +11,9 @@ function Navbar() {
         <img src="../public/images/hamburger-menu.svg" alt="This is a hamburguer menu image" className="w-7 cursor-pointer" />
         <img src="../public/images/logo-minga.svg" alt="This is the logo of minga web page" className='w-7' />
       </nav>
-
-      {display && (<div className="drawer">
-                <p onClick={()=>setDisplay(!display)} className="close">X</p>
-                <p className="option">HOME</p>
-                <p className="option">SIGN IN</p>
-                <p className="option">REGISTER</p>
-        </div>)}
+    
+    {display && (<Display display={display} setDisplay={setDisplay}/>)}
+      
 
 
     </>
